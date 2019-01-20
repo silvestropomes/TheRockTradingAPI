b87f828765c4030e79a11faf150da3fb938343e9
silvestro.pomes <silvestro.pomes@gmail.com>
2019-01-20 09:44:13 +0100

    broken because .net framework -> .net core incompatibility

--- TheRockTradingAPI/Bootstrapper.cs
+++ TheRockTradingAPI/Bootstrapper.cs
@@ -7,6 +7,8 @@ using TheRockTradingAPI.restUtils;
 using TheRockTradingAPI.request;
 using TheRockTradingAPI.contract;
 using System.IO;
+using System.Net.Http;
+using BrakePedal;
 
 namespace TheRockTradingAPI
 {
@@ -18,8 +20,11 @@ namespace TheRockTradingAPI
         {
             var container = new Container();
             container.Register<ApiConfig>(Lifestyle.Singleton);
-            container.Register<IRestCaller, ThrottledRestCaller>(Lifestyle.Singleton);
+            container.Register<IRestCaller, BrakedRestCaller>(Lifestyle.Singleton);
+            container.Register<IThrottleRepository>(() => new MemoryThrottleRepository(), Lifestyle.Singleton);
+            //container.Register<IThrottleRepository, MemoryThrottleRepository>(Lifestyle.Singleton);
             container.Register<RestCaller>(Lifestyle.Singleton);
+            //container.Register<HttpClient>(Lifestyle.Singleton);
             container.Register<TickerRequest>();
             container.Register<BalancesRequest>();
             container.Register<ITheRockRequestFactory>(() => new TheRockRequestFactory(container), Lifestyle.Singleton);
---
+++ TheRockTradingAPI/restUtils/BrakedRestCaller.cs
@@ -0,0 +1,81 @@
+﻿using BrakePedal;
+using BrakePedal.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TheRockTradingAPI.contract;
+
+namespace TheRockTradingAPI.restUtils
+{
+    internal class BrakedRestCaller : IRestCaller, IDisposable
+    {
+        private readonly RestCaller restCaller;
+        
+        public BrakedRestCaller(ApiConfig apiConfig, IThrottleRepository throttleRepository)
+        {
+            var apiRequestPolicy = new HttpThrottlePolicy<IpRequestKey>(throttleRepository)
+            {
+                Name = "Requests",
+                Prefixes = new[] { "requests" },
+
+                PerSecond = 1 // Only allow 10 requests per second per IP
+            };
+
+            DelegatingHandler throttleHandler = new ThrottleHandler(apiRequestPolicy) { InnerHandler = new HttpClientHandler() };
+
+            var httpClient = new HttpClient(throttleHandler);
+            this.restCaller = new RestCaller(apiConfig, httpClient);
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string uri) where T : IResponse
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync<T>(string uri) where T : IResponse
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+
+    public class IpRequestKey : HttpRequestKey
+    {
+        public override void Initialize(HttpRequestMessage request)
+        {
+            base.Initialize(request);
+
+            string forwardedFor = "X-Forwarded-For";
+            if (request.Headers.Contains(forwardedFor))
+            {
+                // Use the forwarded IP address if sitting behind a load balancer
+                string ip = request.Headers.GetValues(forwardedFor).First().Trim();
+
+                // The base class HttpRequestKey gets the IP from other sources.
+                ClientIp = IPAddress.Parse(ip);
+            }
+        }
+
+        public override object[] Values
+        {
+            get
+            {
+                return new object[]
+                {
+                ClientIp
+                };
+            }
+        }
+    }
+}
